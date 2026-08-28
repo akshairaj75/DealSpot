8c59b7b731f8a49dadc84c47104f1573cb17adee
Akshai raj <akshairaj75@gmail.com>
2026-08-28 09:39:11 +0530

    Offer badge enum update

--- src/main/java/com/backend/dealspot/enums/OfferBadgeType.java
+++ src/main/java/com/backend/dealspot/enums/OfferBadgeType.java
@@ -1,10 +1,14 @@
 package com.backend.dealspot.enums;
 
 public enum OfferBadgeType {
+    NONE,
     PERCENT_OFF,
     FLASH,
     NEW,
     BOGO,
     CLEARANCE,
-    COUPON
+    COUPON,
+    FEATURED,
+    PROMO
 }
+
